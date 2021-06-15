2c45188ece64245b7b1c8ccb57ce31ed37cdaae4
kangjw <71870130+kangjwme@users.noreply.github.com>
2021-06-15 16:27:58 +0800

    Update haproxy.sh

--- haproxy.sh
+++ haproxy.sh
@@ -6,4 +6,5 @@ export EXPOSED_PORT=$1
 export BACKEND_HOST=$2":"$3
 curl -fsSL git.io/deploy-google-bbr.sh | bash
 curl -fsSL git.io/deploy-haproxy.sh | sudo -E bash
-echo "You can now use '${INSTANCE_IPV4}:${EXPOSED_PORT}' to connect to your Minecraft server."
+echo "使用 '${INSTANCE_IPV4}:${EXPOSED_PORT}' 來連結你的伺服器吧"
+echo "請記得在後端BungeeCord 資料夾找到 config.yml 設定檔，把「proxy_protocol」從「false」改成「true」"
