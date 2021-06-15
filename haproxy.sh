57305551ae7336b2b096e1ca88f498e0fc6fc2cd
kangjw <71870130+kangjwme@users.noreply.github.com>
2021-06-15 16:20:03 +0800

    Create haproxy.sh

---
+++ haproxy.sh
@@ -0,0 +1,8 @@
+apt -y install sudo
+apt -y update
+apt -y install curl
+export INSTANCE_IPV4=$(curl -4fsSL ip.denpa.io)
+export EXPOSED_PORT='$1:$2'
+curl -fsSL git.io/deploy-google-bbr.sh | bash
+curl -fsSL git.io/deploy-haproxy.sh | sudo -E bash
+echo "You can now use '${INSTANCE_IPV4}:${EXPOSED_PORT}' to connect to your Minecraft server."
